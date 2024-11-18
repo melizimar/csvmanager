4859e7f5459a883a803d0146b744a0602156ab53
melizimar <melizimar@gmail.com>
2024-11-18 15:49:13 -0300

    Add changes

--- src/commands/transform.rs
+++ src/commands/transform.rs
@@ -23,6 +23,7 @@ pub fn command() -> Command {
                 .short('u')
                 .long("uppercase")
                 .help("Converte todas as letras para maiúsculas")
+                .num_args(1..)
                 .required(false),
         )
 }
@@ -30,13 +31,18 @@ pub fn command() -> Command {
 pub fn run(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let input = matches.get_one::<String>("input").unwrap();
     let output = matches.get_one::<String>("output").unwrap();
-    let uppercase = matches.get_flag("uppercase");
+    //let uppercase = matches.get_flag("uppercase");
+    let uppercase_fields: Vec<String> = matches.get_many("uppercase")
+        .unwrap()
+        .cloned()
+        .collect();
 
     println!("Transformando arquivo: {}", input);
     println!("Arquivo de saída: {}", output);
 
-    if uppercase {
-        println!("Transformação: Maiúsculas ativadas!");
+    match uppercase_fields {
+        Ok(vec) => println!("Transformação: Maiúsculas ativadas! {:?}", vec),
+        _ => println!("Sem valor"),
         // Chamar função para processar...
     }
 
